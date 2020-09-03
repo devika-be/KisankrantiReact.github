0a3089a9112b4960ee784dc00de9aa2c0edcc255
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2020-09-03 11:27:01 +0530

    I was changed the style of the text

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -63,58 +63,87 @@ function Register() {
         >
           <div className="container">
             <div className="row">
-              <div className="col-lg-4 col-md-6 mx-auto">
+              <div className="col-lg-5 col-md-6 mx-auto">
                 <div className="card card-login">
                   <form className="form" onSubmit={handleSubmit}>
                     <div className="card-header card-header-success text-center">
                       <h4 className="card-title">Register To Kisankranti</h4>
                     </div>
                     <div className="card-body">
-                      <div className="input-group">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text"></span>
+    
+                      <div>
+                        <div className="col-lg-12 col-sm-6">
+                          <div className="form-group has-success">
+                            <label htmlFor="exampleInput" className="bmd-label-floating">First Name</label>
+                            <input type="text" className="form-control" />
+                          </div>
                         </div>
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="First Name"
-                          {...bindFirstName}
-                        />
-                      </div>
-                      <div className="input-group">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text"></span>
+                        <div className="col-lg-12 col-sm-6">
+                          <div className="form-group has-success">
+                            <label htmlFor="exampleInput" className="bmd-label-floating">Last Name</label>
+                            <input type="text" className="form-control" />
+                          </div>
                         </div>
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Last Name"
-                          {...bindLastName}
-                        />
-                      </div>
-                      <div className="input-group">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text"></span>
+                        <div className="col-lg-12 col-sm-6">
+                          <div className="form-group has-success">
+                            <label htmlFor="exampleInput" className="bmd-label-floating">Mobile Number</label>
+                            <input type="text" className="form-control" />
+                          </div>
                         </div>
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Mobile Number"
-                          {...bindPhoneNumber}
-                        />
-                      </div>
-                      <div className="input-group">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text"></span>
+                        <div className="col-lg-12 col-sm-2">
+                          <div className="form-group has-success">
+                            <label htmlFor="exampleInput" className="bmd-label-floating">Password</label>
+                            <input type="password" className="form-control" autoComplete />
+                          </div>
                         </div>
-                        <input
-                          type="password"
-                          className="form-control"
-                          placeholder="Password"
-                          {...bindPassword}
-                        />
                       </div>
-                    </div>
+    
+    
+//                       <div className="input-group">
+//                         <div className="input-group-prepend">
+//                           <span className="input-group-text"></span>
+//                         </div>
+//                         <input
+//                           type="text"
+//                           className="form-control"
+//                           placeholder="First Name"
+//                           {...bindFirstName}
+//                         />
+//                       </div>
+//                       <div className="input-group">
+//                         <div className="input-group-prepend">
+//                           <span className="input-group-text"></span>
+//                         </div>
+//                         <input
+//                           type="text"
+//                           className="form-control"
+//                           placeholder="Last Name"
+//                           {...bindLastName}
+//                         />
+//                       </div>
+//                       <div className="input-group">
+//                         <div className="input-group-prepend">
+//                           <span className="input-group-text"></span>
+//                         </div>
+//                         <input
+//                           type="text"
+//                           className="form-control"
+//                           placeholder="Mobile Number"
+//                           {...bindPhoneNumber}
+//                         />
+//                       </div>
+//                       <div className="input-group">
+//                         <div className="input-group-prepend">
+//                           <span className="input-group-text"></span>
+//                         </div>
+//                         <input
+//                           type="password"
+//                           className="form-control"
+//                           placeholder="Password"
+//                           {...bindPassword}
+//                         />
+//                       </div>
+//                     </div>
                     <div className="footer text-center">
                       <button
                         type="submit"
@@ -123,6 +152,7 @@ function Register() {
                         Submit<Link to="/OTP"></Link>
                       </button>
                     </div>
+                    <br />
                   </form>
                 </div>
               </div>
